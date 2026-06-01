93201724725815ca740e7d3a2e1aa78b44e6ab74
Puput Agustin <puputagustin581@students.unnes.ac.id>
2026-06-01 21:42:51 +0700

    Add inventory transaction models

--- app/Models/Asset.php
+++ app/Models/Asset.php
@@ -13,17 +13,31 @@ class Asset extends Model
         'location',
         'condition',
         'merk',
+        'warna',
+        'ukuran',
+        'satuan',
         'penanggungjawab',
         'tanggal_masuk',
         'harga',
+        'stok_awal',
+        'stok_saat_ini',
+
         ];
 
     protected $primaryKey = 'code';
     public $incrementing = false;
     protected $keyType = 'string';
 
-    public function getRouteKeyName(){
-    return 'code';}
+    public function getRouteKeyName()
+    {
+        return 'code';
+    }
+
+     public function transactions()
+    {
+        return $this->hasMany(AssetTransaction::class, 'asset_code', 'code');
+    }
+
 }
 
 
---
+++ app/Models/AssetTransaction.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AssetTransaction extends Model
+{
+    protected $fillable = [
+        'asset_code',
+        'user_id',
+        'type',
+        'quantity',
+        'note',
+        'transaction_date',
+    ];
+
+    protected $casts = [
+        'transaction_date' => 'datetime',
+    ];
+
+    public function asset()
+    {
+        return $this->belongsTo(Asset::class, 'asset_code', 'code');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
