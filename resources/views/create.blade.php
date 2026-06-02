849ff78ebd7c8f4c27907ad8ac6626fa44eb88eb
Puput Agustin <puputagustin581@students.unnes.ac.id>
2026-06-02 10:10:36 +0700

    Add inventory fields to asset forms

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -772,27 +772,27 @@ input.is-invalid {
                             </div>
 
                             <div class="form-group">
-                                <label for="warna">Warna</label>
+                                <label class="form-label">warna</label>
                                 <input type="text" name="warna" id="warna" value="{{ old('warna') }}" placeholder="Contoh: Hitam">
                             </div>
 
                             <div class="form-group">
-                                <label for="ukuran">Ukuran</label>
+                                <label class="form-label">Ukuran</label>
                                 <input type="text" name="ukuran" id="ukuran" value="{{ old('ukuran') }}" placeholder="Contoh: M / L / XL">
                             </div>
 
                             <div class="form-group">
-                                <label for="satuan">Satuan</label>
+                                <label class="form-label">Satuan</label>
                                 <input type="text" name="satuan" id="satuan" value="{{ old('satuan', 'pcs') }}" placeholder="Contoh: pcs">
                             </div>
 
                             <div class="form-group">
-                                <label for="stok_awal">Stok Awal</label>
+                                <label class="form-label">Stok Awal</label>
                                 <input type="number" name="stok_awal" id="stok_awal" value="{{ old('stok_awal', 0) }}" min="0">
                             </div>
 
                             <div class="form-group">
-                                <label for="stok_saat_ini">Stok Saat Ini</label>
+                                <label class="form-label">Stok Saat Ini</label>
                                 <input type="number" name="stok_saat_ini" id="stok_saat_ini" value="{{ old('stok_saat_ini', 0) }}" min="0">
                             </div>
 
